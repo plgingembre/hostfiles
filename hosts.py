8147228fa2a1610ad3c38640b962ed907e49bfb9
pierregi <pierregi@pierregi-000647.local>
2018-09-11 14:07:47 -0700

    Lab Switch Groups added

--- hosts.py
+++ hosts.py
@@ -27,7 +27,9 @@ TME_ACCTON_SWITCHES = [
     '10.36.10.45',
     '10.36.10.46',
     '10.36.10.47',
-    '10.36.10.48'
+    '10.36.10.48',
+    '10.36.10.20'
+
 ]
 TME_DELL_SWITCHES = [
     '10.36.10.29',
@@ -49,7 +51,6 @@ TME_DELL_SWITCHES = [
 ]
 TME_SOLARIS_SWITCHES = [
     '10.36.10.19',
-    '10.36.10.20',
     '10.36.10.21',
     '10.36.10.22',
     '10.36.10.23',
@@ -58,12 +59,12 @@ TME_SOLARIS_SWITCHES = [
     '10.36.10.26'
 ]
 TME_CISCO_SWITCHES = [
-    '10.36.10.27',
-    '10.36.10.28'
+    #'10.36.10.27',
+    #'10.36.10.28'
 ]
 EBC_BE_SWITCHES = [
-    #'10.38.10.12',
-    #'10.38.10.15'
+    '10.38.10.252',
+    '10.38.10.253'
 ]
 EBC_ACCTON_SWITCHES = [
     '10.38.10.12',
@@ -73,16 +74,20 @@ EBC_ACCTON_SWITCHES = [
     '10.38.10.25',
     '10.38.10.26',
     '10.38.10.31',
-    '10.38.10.32',
-    '10.38.10.34',
-    '10.38.10.35',
-    '10.38.10.36',
     '10.38.10.37',
     '10.38.10.38'
 ]
+EBC_ACCTON_SWITCHES_2 = [
+    '10.38.10.32',
+    '10.38.10.34',
+    '10.38.10.35',
+    '10.38.10.36'
+]
 EBC_DELL_SWITCHES = [
     '10.38.10.19',
-    '10.38.10.20',
+    '10.38.10.20'
+]
+EBC_DELL_SWITCHES_2 = [
     '10.38.10.22',
     '10.38.10.23'
 ]
@@ -90,15 +95,26 @@ DISTRI = [
     '10.134.1.23',
     '10.134.1.24'
 ]
+TESTBED = [
+    '10.36.10.11',
+    '10.36.10.12',
+    '10.36.10.13',
+    '10.36.10.14',
+    '10.36.10.15',
+    '10.36.10.16'
+]
+ALL_SWITCHES = TME_BE_SWITCHES + TME_ACCTON_SWITCHES + TME_DELL_SWITCHES + TME_SOLARIS_SWITCHES + TME_CISCO_SWITCHES + EBC_BE_SWITCHES + EBC_ACCTON_SWITCHES + EBC_DELL_SWITCHES + DISTRI
 SHELL_USERNAME = 'pluribus'
-SHELL_PASSWORD = 'test123'
+SOLARIS_USERNAME = 'admin'
 NETVISOR_USERNAME = 'network-admin'
+#SHELL_PASSWORD = 'test123'
 NETVISOR_DEFAULT_PASSWORD = 'test123'
 NETVISOR_TME_BE_PASSWORD = 'N3T@dM-123!'
 NETVISOR_EBC_BE_PASSWORD = 'Test123'
 TIMEOUT = 60
 PKEY = ''
 PORT = 22
+PLATFORM = 'pluribus'
 #PN_COMMANDS = ['switch-local switch-info-show format switch,model,chassis-serial,system-mem,disk-model,disk-type,disk-firmware', 'switch-local switch-setup-show format device-id']
 #COMMANDS = ['cat /etc/chassis_info | egrep -E "Product Name|Service Tag|Serial Number"']
 #COMMANDS = ['lsblk', 'sudo -S parted /dev/sda print', 'sudo -S parted /dev/sdb print']
